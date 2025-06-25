7e895fae941f895aefa9479b2a24a024847af11d
Shrey Raj <shreyraj1653@gmail.com>
2025-06-25 15:32:10 +0530

    add: modify reqs.txt & render-build.sh

--- .render-build.sh
+++ .render-build.sh
@@ -3,3 +3,6 @@ set -o errexit
 
 pip install --upgrade pip
 pip install --prefer-binary -r requirements.txt
+
+# Add this to link the model
+python -m spacy link en_core_web_sm en_core_web_sm --force
