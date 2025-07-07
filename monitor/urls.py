947cf4a4f012afde67fe57b8fae0780f3baa8a8c
Shrey Raj <shreyraj1653@gmail.com>
2025-07-07 11:34:39 +0530

    add: generate_prompts() function

--- monitor/urls.py
+++ monitor/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-# from .views import generate_prompts
+from .views import generate_prompts
 from .views import brand_mention_score
 from .views import job_status
 from .views import health_check
 urlpatterns = [
-    # path('generatePrompts/', generate_prompts),
+    path('generatePrompts/', generate_prompts),
     path('getMentionScore/', brand_mention_score),
     path('getJobStatus/', job_status),
     path('healthcheck/', health_check)
